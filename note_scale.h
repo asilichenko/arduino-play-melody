8d25088edccebb5d5b80449e81137761e3a9f0c4
Oleksii Sylichenko <a.silichenko@gmail.com>
2023-12-30 08:12:58 +0200

    Update note_scale.h
    add in comments notes of the octaves 5 and 6
    make variables const
    make names upper case

--- note_scale.h
+++ note_scale.h
@@ -22,9 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-int note_scale_len = 17;
+const int NOTE_SCALE_LEN = 17;
 
-String note_names[] = {
+const String NOTE_NAMES[NOTE_SCALE_LEN] = {
+  // Octave 5: "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5"
+  // Octave 6: "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6"
+
   "E5", "F#5", "G#5", "A5", "B5", "C#6", "D6", "E6", "F#6", // jingle bells
   "C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6" // merry christmas
 };
@@ -35,7 +38,10 @@ String note_names[] = {
  * @see https://en.wikipedia.org/wiki/A440_(pitch_standard)
  * @see https://pages.mtu.edu/~suits/notefreqs.html
  */
-float note_tones[] = {
+const int NOTE_TONES[NOTE_SCALE_LEN] = {
+  // Octave 5: 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77
+  // Octave 6: 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53
+
   659.25, 739.99, 830.61, 880.00, 987.77, 1108.73, 1174.66, 1318.51, 1479.98, // jingle bells
   523.25, 587.33, 659.25, 698.46, 783.99, 880.00, 987.77, 1046.50 // merry christmas
 };
